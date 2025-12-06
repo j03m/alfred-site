d237d154fef1a8223496d0c958a6bad0bf3699a0
j03m <jmordetsky@gmail.com>
2025-12-06 11:26:33 -0500

    .github stuff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Use this if deploying to https://<USERNAME>.github.io/<REPO_NAME>
+  // basePath: '/alfred-analysis-site',
 };
 
 export default nextConfig;
